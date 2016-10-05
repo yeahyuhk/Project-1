cd34f0f95c22d4b4392c47b782ad49dd2fe796c4
Yahya Khan <yahyas.email@gmail.com>
2016-10-05 00:40:43 -0400

    updated code in file 10/5/16
    added array for rect heights
    randomized heights
    old code commented out

--- yahya_project1/sketch.js
+++ yahya_project1/sketch.js
@@ -1,24 +1,27 @@
 function setup() {
   createCanvas(900, 600);
-  frameRate(30);
+  frameRate(2);
 }
 
-
+var sizes = [160, 230, 390, 440, 600, 540, 390, 280, 120]
 
 
 function soundBar(rx, rheight) {
   rectMode(CENTER);
-  rect(rx, height, 70, rheight-2*mouseY+100);
+  //rect(rx, height, 70, rheight-2*mouseY+100);
+  rect(rx, height, 70, rheight);
 }
 
 
 
 
 function draw() {
+  makeRect = 0;
+  var rectY = 50
   background(0);
   noStroke();
   fill(230, 180, 0);
-  soundBar(50, 160);
+  /* soundBar(50, 160);
   soundBar(150, 230);
   soundBar(250, 390);
   soundBar(350, 440);
@@ -27,4 +30,11 @@ function draw() {
   soundBar(650, 390);
   soundBar(750, 280);
   soundBar(850, 120);
+  */
+  for (var makeRect = 0; makeRect < 9; makeRect++) {
+    var i = int(random(sizes.length));
+    soundBar(rectY, sizes[i]);
+    rectY += 100;
+  }
+
 }
